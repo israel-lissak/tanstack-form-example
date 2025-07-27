9068e5892c895f77e966812ecfb1a2aaf855fcf0
Israel Lissak <islissak@gmail.com>
2025-07-27 17:23:31 +0300

    Added styling

--- src/features/people/person-fieldes.tsx
+++ src/features/people/person-fieldes.tsx
@@ -13,7 +13,7 @@ export const FieldGroupPerson = withFieldGroup({
     },
     render: function Render({ group, title }) {
         return (
-            <>
+            <div className="flex flex-col  p-4 bg-white shadow rounded mb-4">
                 <h3>{title}</h3>
                 <group.AppField
                     name="name"
@@ -23,7 +23,7 @@ export const FieldGroupPerson = withFieldGroup({
                     name="age"
                     children={(field) => <field.NumberField label="Age" />}
                 />
-            </>
+            </div>
         );
     },
 });
